81756171a0615ca77578334021cf946c4fb0dc2c
cdadar <gunbanmi@gmail.com>
2019-07-29 22:58:44 +0800

    update rxvt

--- openSUSE-One-Click-Installer.sh
+++ openSUSE-One-Click-Installer.sh
@@ -234,6 +234,8 @@ sudo zypper -n in -l filezilla
 
 sudo zypper -n in -l xterm
 
+sudo zypper -n in -l rxvt-unicode urxvt-font-size urxvt-perls
+
 sudo zypper -n in -l mu4e
 
 sudo zypper -n in -l mutt getmail msmtp procmail fetchmail
