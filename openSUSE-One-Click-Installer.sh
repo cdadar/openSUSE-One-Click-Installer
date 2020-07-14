e49e34f76b2ec88203c1e47278a03f89a5ea8cd7
cdadar <gunbanmi@gmail.com>
2020-07-14 13:44:38 +0800

    add remmina

--- openSUSE-One-Click-Installer.sh
+++ openSUSE-One-Click-Installer.sh
@@ -353,6 +353,9 @@ sudo zypper -n in -l freeplane
 sudo zypper -n in -l direnv
 
 sudo zypper -n in -l net-tools-deprecated
+
+sudo zypper -n in -l remmina
+
 # 禁用 leap 软件检查更新
 #sudo zypper remove plasma5-pk-updates
 #sudo zypper addlock plasma5-pk-updates
