7d6af49b78ef20d64c130ca2396dc98e5c9c08aa
cdadar <gunbanmi@gmail.com>
2020-09-24 09:39:01 +0800

    update rzsz

--- openSUSE-One-Click-Installer.sh
+++ openSUSE-One-Click-Installer.sh
@@ -360,6 +360,8 @@ sudo zypper -n in -l calibre
 
 sudo zypper -n in -l inotify-tools
 
+sudo zypper -n in -l rzsz
+
 # 禁用 leap 软件检查更新
 #sudo zypper remove plasma5-pk-updates
 #sudo zypper addlock plasma5-pk-updates
